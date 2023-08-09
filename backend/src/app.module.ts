0f8ca42c2aa629f453969d0520c02100810a07db
gustavooavila <gustavooavila@gmail.com>
2023-08-08 21:39:03 -0300

    install and register basic data input validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {provide: APP_PIPE, useClass: ValidationPipe}],
 })
 export class AppModule {}
